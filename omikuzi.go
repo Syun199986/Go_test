05f7b46c710efd55fbd581bfbc7d82007db963d6
Syun199986 <syun990806@gmail.com>
2023-11-14 13:35:02 +0900

    omikuzi.go finished

--- omikuzi.go
+++ omikuzi.go
@@ -1,16 +1,24 @@
 package main
+
 import (
 	"fmt"
-	"strconv"
+	"math/rand"
+	"time"
 )
 
 func Omikuzi() {
-	for i := 1; i <= 100; i++ {
-		str_i := strconv.Itoa(i)
-		if i % 2 == 0 {
-			fmt.Println(str_i + "-偶数")
-		} else {
-			fmt.Println(str_i + "-奇数")
-		}
+	seed := time.Now().UnixNano() // 現在時刻を数値で取得
+	rand.New(rand.NewSource(seed)) // 乱数の種を設定
+	val := rand.Intn(6) + 1 // 1~6の範囲で乱数生成
+	
+	switch {
+	case val == 1:
+		fmt.Println("凶")
+	case val == 2 || val == 3:
+		fmt.Println("吉")
+	case val == 4 || val == 5:
+		fmt.Println("中吉")
+	case val == 6:
+		fmt.Println("大吉")
 	}
 }
